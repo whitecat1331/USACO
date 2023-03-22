e944c5c7c323fd1d60310b0cbbb1fe40ca7586ea
Patrick Loyd <patrickloyd@pop-os.localdomain>
2023-03-22 19:11:14 -0400

    start wertyu solution

---
+++ examples/wertyuz_decoding_solution.py
@@ -0,0 +1,16 @@
+# create 2d array for each key on laptop
+qwerty_keys = [
+            ['~','!','@','#','$','^','&','*','(',')','_','+'],
+            ['1','2','3','4','5','6','7','8','9','0','-','='],
+            ['q','w','e','r','t','y','u','i','o','p','[',']','\\'],
+            ['a','s','d','f','g','h','j','k','l',';', '\'',],
+            ['z','x','c','v','b','n','m',',','.','/']
+        ]
+
+# iterate through each value 
+
+# check if current letter matches
+
+# move letter if not first
+
+

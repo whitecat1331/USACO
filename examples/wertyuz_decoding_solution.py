f04b603e01cd65c536fb452e65478e71ee7bc43c
Patrick Loyd <patrickloyd@pop-os.localdomain>
2023-03-24 12:57:01 -0400

    replan solution

--- examples/wertyuz_decoding_solution.py
+++ examples/wertyuz_decoding_solution.py
@@ -3,14 +3,28 @@ qwerty_keys = [
             ['~','!','@','#','$','^','&','*','(',')','_','+'],
             ['1','2','3','4','5','6','7','8','9','0','-','='],
             ['q','w','e','r','t','y','u','i','o','p','[',']','\\'],
-            ['a','s','d','f','g','h','j','k','l',';', '\'',],
+            ['a','s','d','f','g','h','j','k','l',';', '\''],
             ['z','x','c','v','b','n','m',',','.','/']
         ]
+excluded_keys = [first_key[0] for first_key in qwerty_keys]
+sample_input = "O S, YPFSU/"
+expected_result = "I AM FINE TODAY."
 
 # iterate through each value 
-
+for letter in sample_input:
+    for row in qwerty_keys:
+        for key in row:
+            if letter in excluded_keys:
+                pass
+            elif letter in row:
+                letter = letter
 # check if current letter matches
+        
 
 # move letter if not first
 
-
+# iterate through letter
+# check if current letter is first key
+# check if first key and keep same
+# reset the letter to the previous key
+# append the letter to a new buffer string

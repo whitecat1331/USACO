862d7e29c8aa014da967204b14f0aa321dcc55dd
whitecat1331 <patrickloyd789@gmail.com>
2023-04-07 18:05:46 -0400

    start dictionary prompt and examples

--- lesson_6/l6_data_structures_2.py
+++ lesson_6/l6_data_structures_2.py
@@ -130,4 +130,91 @@ q.put(1)
 sys.stdout.write("Empty: "+ str(q.empty()) + '\n' )
 sys.stdout.write("Full: "+ str(q.full()) + '\n\n')
 
+# Dictionaries 
+sys.stdout.write("Dictionaries\n")
+dictionary_prompt = """A dictionary in Python is a built-in data type that allows for key value pairs.
+Dictionaries are useful for containing data that is grouped in pairs.
+There are many practical uses for a dictionary in Python.
+Dictionaries can be indexed by key to return the grouped value. 
+A key in python can either be a String or a number type. 
+The value of a dictionary can hold any type of object or value.
+
+Dictionary Declaration:
+Dictionaries are declared using curly braces {}. 
+The key and values are sperated by a colon :.
+Each key-value pair is sperated with a comma ,.
+
+Example: 
+    my_dict = {"key": "value", "another_key": "another_value"}
+
+The value of a dictionary can be accessed by key
+Example:
+print(my_dict["key"]) # prints value
+my_dict["key"] = "some_value"
+print(my_dict["key"]) # prints some_value
+
+Use Cases:
+When working with APIs, Python uses dictionaries to model JSON objects.
+They are useful anytime you have a key value pair you are trying to save in Python
+
+"""
+
+sys.stdout.write(dictionary_prompt)
+
+# Dictionary Indexing
+sys.stdout.write("Dictionary Indexing\n")
+advengers = {"Spider-Man": "Peter Parker", "Iron-Man": "Tony Stark","Hulk": "Bruce Banner"}
+
+US_currency = {
+        "penny": .01,
+        "nickel": .05,
+        "dime": .10,
+        "quarter": .25,
+        "half-dollar": .50,
+        "dollar": 1.00
+                }
+
+
+
+sys.stdout.write(f"advengers: {advengers}\nUS_currency: {US_currency}\n\n")
+
+
+# Nested Dictionaries
+sys.stdout.write("Nested Dictionaries\n")
+ranked_advengers = {
+        1: {"first_name": "Tony", "last_name": "Stark", "alias": "Iron-Man", "abilities": ["propultion thrusters", "plasma shot", "Jarvis"]},
+        2: {"first_name": "Peter", "last_name": "Parker", "alias": "Spider-Man", "abilities": ["spin webs", "swing", "spidey sense"]},
+        3: {"first_name": "Bruce", "last_name": "Banner", "alias": "Hulk", "abilities": ["smash", "smash", "smash"]}
+        }
+hulk_dict = ranked_advengers[3]
+hulk_abilities = hulk_dict["abilities"]
+hulk_first_ability = hulk_abilities[0]
+nested_dicts_prompt = f"""Dictionaries can hold any type of object as their value.
+This means that dictionaries can also hold any container type as their value, including other dictionaries.
+If the value you initially index into is another container type, simply index into that type using that notation to index 
+into the individual value within the entire dictionary.
+There is not limit to the amount of information that is nested within a dictionary, in fact some JSON dictionaries can have five or six nested items.
+With so much nesting, it can be easy to get lost in a large dictionary.
+Luckily, python gives us other tools to effectively navigate a dictionary in our program.
+
+
+Most manuel dictionary indexing is done with the developer knowing the values to index to.
+If the developer created the dictionary, then they should know how to index into it.
+If a developer is working with a large dictionary that was externally imported into the program,
+then reading the documentation associated with the imported data would be the best way to find what keys to index to.
+
+Hulk Dictionary: {ranked_advengers[3]}
+Hulk Abilities: {ranked_advengers[3]["abilities"]}
+Hulks First Ability: {ranked_advengers[3]["abilities"][0]}
+
+Each object can also be saved in a variable
+
+Hulk Dictionary: {hulk_dict}
+Hulk Abilities: {hulk_abilities}
+Hulks First Ability: {hulk_first_ability}
+
+"""
+sys.stdout.write(nested_dicts_prompt)
+
+
 

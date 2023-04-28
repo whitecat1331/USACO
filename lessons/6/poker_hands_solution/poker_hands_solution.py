04fd935624771c3755694af597c0bf8d0e80bc3c
whitecat1331 <patrickloyd789@gmail.com>
2023-04-28 18:59:14 -0400

    start conversion of 1D cards to games

--- lessons/6/poker_hands_solution/poker_hands_solution.py
+++ lessons/6/poker_hands_solution/poker_hands_solution.py
@@ -21,8 +21,8 @@ class Deck:
             for value in Card.VALUES:
                 self.deck.append(Card(value, suit))
 
-    def get_deck(self):
-        return self.deck
+    def get_card(self, i):
+        return self.deck[i]
     
     def add_card(self, card):
         self.deck.append(card)
@@ -31,7 +31,8 @@ class Deck:
         for card in self.deck:
             card.print_card_info()
 
-class PokerHand():
+
+class Player():
     def __init__(self, name):
         self.deck = Deck()
         self.name = name
@@ -44,33 +45,78 @@ class PokerHand():
         self.deck.print_cards()
 
 
-class PokerHandsSolution:
-    def __init__(self, file_of_hands):
-        player1 = PokerHand("Black")
-        player2 = PokerHand("White")
-        all_players = (player1, player2)
+class PokerRulesBook:
+    def __init__(self, player1, player2, cards_per_game):
+        self.player1 = player1 
+        self.player2 = player2
+        self.cards_per_game = cards_per_game
+        self.all_players = (player1, player2)
+        self.games = []
+        self.split_games()
 
-        with open(file_of_hands, 'r') as f:
+    def split_games(self):
+        for player in self.all_players:
+            current_game_cards = []
+
+            for i in range(self.cards_per_game):
+                if (len(current_game_cards) % self.cards_per_game) == 0:
+                    self.games.append(current_game_cards)
+                    current_game_cards = []
+                current_game_cards.append(player.deck.get_card(i))
+
+    def print_cards(self):
+        for player in self.all_players:
+            player.print_cards()
+
+    def print_games(self):
+        game_count = 0
+        for game in self.games:
+            sys.stdout.write(f"Game: {game_count}: {game}")
+            game_count += 1
+
+
+
+class PokerHandsSolution:
+    def __init__(self, input_file):
+        self.player1 = Player("Black")
+        self.player2 = Player("White")
+        self.input_file = input_file
+        self.all_players = (self.player1, self.player2)
+        self.read_input_file()
+
+    def read_input_file(self):
+        with open(self.input_file, 'r') as f:
             for hands in f:
                 line_of_cards = hands.split()
                 card_count = 0
                 for input_card in line_of_cards:
                     card = Card(input_card[0],input_card[1])
                     if (card_count % 10) < 5:
-                        player1.add_card(card)
+                        self.player1.add_card(card)
                     else:
-                        player2.add_card(card)
+                        self.player2.add_card(card)
 
                     card_count += 1
             
-        for player in all_players:
+    def testing(self):
+        for player in self.all_players:
             player.print_cards()
-        sys.stdout.write('\n')
+            sys.stdout.write('\n')
+        poker_rule_book = PokerRulesBook(self.player1, self.player2, 5)
+        poker_rule_book.print_games()
+
+            
+
+
+
+
 
 
 
 INPUT_FILE = "sample_input.txt"
 poker_hands_solution = PokerHandsSolution(INPUT_FILE)    
+poker_hands_solution.testing()
+
 
 
 
